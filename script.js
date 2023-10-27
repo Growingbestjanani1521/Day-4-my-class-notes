e90ed33cfad0032f864b72654840e5203bb8596e
Growingbestjanani1521 <janananimani1590@gmail.com>
2023-10-27 13:27:16 +0530

    class note updated

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all",true)
-request.send();
-request.onload = function(){
-    var data = request.response;
-    //console.log(data);
-    var result = JSON.parse(data);
-    console.log(result);
-    //console.log(result[0].name.common);
-    console.log(result[249].area);
+// var request = new XMLHttpRequest();
+// request.open("GET","https://restcountries.com/v3.1/all",true)
+// request.send();
+// request.onload = function(){
+//     var data = request.response;
+//     //console.log(data);
+//     var result = JSON.parse(data);
+//     console.log(result);
+//     //console.log(result[0].name.common);
+//     console.log(result[249].area);
     
-    for(var i=0;i<result.length;i++){
-       // console.log(result[i].area);
-    }
-}
+//     for(var i=0;i<result.length;i++){
+//        // console.log(result[i].area);
+//     }
+// }
 
 
 /*var a =10;
